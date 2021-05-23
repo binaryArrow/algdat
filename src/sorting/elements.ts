ea489522bf8def891642217f9a97ae8ded8a6686
binaryArrow <cancihan96@gmail.com>
2021-05-24 00:37:52 +0200

    drawing bars TODO: draw exactly the amount of bars that fill the canvas with right width

--- src/sorting/elements.ts
+++ src/sorting/elements.ts
@@ -2,24 +2,19 @@ import * as P5 from "p5";
 
 export class Elements {
   p5: P5
-  pos: P5.Vector
-  size: number
+  values: number[]
 
-  constructor(p5:P5, atPosition: P5.Vector,size: number ) {
+  constructor(p5:P5, values: number[] ) {
     this.p5 = p5
-    this.pos = atPosition
-    this.size = size
+    this.values = values
   }
 
-  draw() {
+  draw(i: number) {
     const p5 = this.p5
-    p5.push()
-    p5.translate(this.pos)
-    p5.noStroke()
-    p5.fill("orange")
-    p5.ellipse(0, 0, this.size)
-
-    p5.pop()
-
+    p5.stroke(100, 143, 143)
+    p5.fill(40)
+    p5.rect(i * 8, 500, 8, - this.values[i], 20)
   }
+
+
 }
--- src/sorting/insertion-sort.ts
+++ src/sorting/insertion-sort.ts
@@ -2,30 +2,37 @@ import * as P5 from "p5"
 import {Elements} from "./elements";
 
 export class InsertionSort {
-  private elements: Elements[] = []
+  private heading = "insertion-sort"
+  private elements: Elements
 
   attached() {
     const sketch = s => {
+      let values = new Array<number>(740)
       s.setup = () => {
-        const canvas = s.createCanvas(200, 200)
+        const canvas = s.createCanvas(740, 500)
         canvas.parent("sketch-holder")
-        s.background("white")
+        s.background("grey")
 
-        for (let i = 1; i < 4; i++) {
-          const p = s.width / 4;
-          const circlePos = s.createVector(p * i, s.height / 2);
-          const size = i % 2 !== 0 ? 24 : 32;
-          this.elements.push(new Elements(s, circlePos, size));
+        // for (let i = 1; i < 4; i++) {
+        //   const p = s.width / 4;
+        //   const circlePos = s.createVector(p * i, s.height / 2);
+        //   const size = i % 2 !== 0 ? 24 : 32;
+        //   this.elements.push(new Elements(s, circlePos, size));
+        // }
+        for(let i = 0; i < values.length; i++){
+          values[i] = Math.floor(Math.random() * 500)
         }
+        this.elements = new Elements(s, values)
       }
 
       s.draw = () => {
-        this.elements.forEach(element => element.draw())
+        for ( let i = 0; i < values.length; i++) {
+          this.elements.draw(i)
+        }
       }
     }
     console.log(sketch)
       new P5(sketch)
   }
-
 }
 
