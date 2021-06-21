4686a09866c70cd61a247eb90a9136a015ed1170
binaryArrow <cancihan96@gmail.com>
2021-06-21 23:54:15 +0200

    randomized bars. TODO: Make the directions...

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,13 @@ import {Router, RouterConfiguration} from 'aurelia-router';
 import {bootstrap} from "aurelia-bootstrapper";
 
 export class App {
+  private bars: number[] = []
+
+  constructor(){
+    for(let i=0; i<=9; i++){
+      const temp: number = Math.floor(Math.random()* (Math.floor(10) - (Math.ceil(1)))) + Math.ceil(1)
+      this.bars.push(temp)
+    }
+  }
 
 }
