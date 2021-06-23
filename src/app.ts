ce89b7515f8c28ef7596cd9430b9cd4280be81f7
binaryArrow <cancihan96@gmail.com>
2021-06-23 02:35:06 +0200

    added class Bars to position bars

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import {PLATFORM} from 'aurelia-pal';
 import {Router, RouterConfiguration} from 'aurelia-router';
 import {bootstrap} from "aurelia-bootstrapper";
+import {Bars} from "./bars";
 
 export class App {
-  private bars: number[] = []
+  private bars: Bars[] = []
   private svgWidth: number
   private svgHeight: number
   barWidth = 50
@@ -12,11 +13,12 @@ export class App {
     const maxValue = 600
     const howManyBars = 10
     for(let i=1; i<=howManyBars; i++){
-      const temp: number = Math.floor(Math.random() * maxValue + 1)
-      this.bars.push(temp)
+      const value: number = Math.floor(Math.random() * maxValue + 1)
+      const bar = new Bars(value, (i - 1)*this.barWidth, this.barWidth)
+      this.bars.push(bar)
     }
     this.svgHeight = maxValue + 1
-    this.svgWidth = this.barWidth * howManyBars
+    this.svgWidth = this.barWidth * howManyBars + this.barWidth
   }
 
 }
