18680f885eb9613be8c1da0f68685833685a4d45
binaryArrow <cancihan96@gmail.com>
2021-07-06 02:13:47 +0200

    added insertionSort

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,13 @@ export class App {
   barWidth // barwidth is relational to howManyBars
   maxValue = 600 // max height of bars
   @observable howManyBars = 10 // using standard convention with the function called has the name with 'changed' in the end EG: howManyBarsChanged()
-  unsorted = 'fill: rgb(166, 67, 67)'
-  sorted = 'fill: cadetblue'
   functionIsRunning = false
-  speed: number
+  speed = 90
+  running = false
 
   constructor(){
     this.svgHeight = this.maxValue + 10
     this.createBars()
-    // this.svgWidth = this.barWidth * this.howManyBars + 2
   }
 
   createBars(): void {
@@ -24,7 +22,7 @@ export class App {
     // creating bars with random heights
     for(let i=1; i<=this.howManyBars; i++){
       const value: number = Math.floor(Math.random() * this.maxValue + 1)
-      const bar = new Bars(value, (i - 1)*this.barWidth+1, this.barWidth, this.unsorted )
+      const bar = new Bars(value, (i - 1)*this.barWidth+1, this.barWidth, 'fill: rgb(166, 67, 67);' )
       this.bars.push(bar)
     }
   }
@@ -49,18 +47,43 @@ export class App {
           const swap = this.bars[j].value;
           this.bars[j].value = this.bars[j + 1].value;
           this.bars[j + 1].value = swap;
-          await new Promise(resolve => {
-            setTimeout(() => {
-              resolve('resolved')
-            }, this.speed)
-          })
         }
       }
-      this.bars[this.bars.length - 1 - i].style = this.sorted
+      this.bars[this.bars.length - 1 - i].style = 'fill: cadetblue;'
+      await new Promise(resolve => {
+        setTimeout(() => {
+          resolve('resolved')
+        }, this.speed)
+      })
     }
     this.functionIsRunning = false
   }
 
-  // TODO: center the bar field.implement insertion-sort
+  async insertionSort(): Promise<void> {
+    this.functionIsRunning = true
+    let j, key
+    for (let i = 1; i < this.bars.length; i++) {
+      key = this.bars[i].value
+      j = i-1
+      while (j >= 0 && this.bars[j].value > key) {
+        this.bars[j+1].value = this.bars[j].value
+        j = j-1
+      }
+      this.bars[j+1].value = key
+      await new Promise(resolve => {
+        setTimeout(() => {
+          resolve('resolved')
+        }, this.speed)
+      })
+    }
+    this.bars.forEach(it => it.style = 'fill: rgb(153, 102, 153);')
+    this.functionIsRunning = false
+  }
+
+
+  cancel(): void {
+    this.running = true
+  }
+  // TODO: insertion-sort
 
 }
